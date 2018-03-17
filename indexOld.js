2dbb14fca2352fe5719822b73f6b319c3ad3cbee
ks ro <ks.romanchik@gmail.com>
2018-03-17 21:12:23 +0300

    add calculating background cover

--- indexOld.js
+++ indexOld.js
@@ -35,58 +35,34 @@ const init = () => {
     new PIXI.filters.BlurFilter(0.3)
   ]
 
-  function imageCover(imageWidth, imageHeight) {
+  catSprite.x = 50
+  catSprite.y = 50
 
-    const heightScale = window.innerHeight / imageHeight
-    const widthScale = window.innerWidth / imageWidth
-
-    const scale = heightScale > widthScale ? heightScale : widthScale
-
-    const heightAnchor = ((imageHeight * scale - window.innerHeight) / (imageHeight * scale)) / 2
-    const widthAnchor = ((imageWidth * scale - window.innerWidth) / (imageWidth * scale)) / 2
-
-    return {
-      scale: {
-        x: heightScale > widthScale ? heightScale : widthScale,
-        y: heightScale > widthScale ? heightScale : widthScale,
-      },
-      anchor: {
-        x: isFinite(widthAnchor) ? widthAnchor : 0,
-        y: isFinite(heightAnchor) ? heightAnchor : 0
-      }
-    }
-  }
-
-  const spriteParams = imageCover(catSprite.width, catSprite.height)
-
-  catSprite.scale.set(spriteParams.scale.x, spriteParams.scale.y)
-  catSprite.anchor.set(spriteParams.anchor.x, spriteParams.anchor.y)
+  catSprite.scale.x = 0.7
+  catSprite.scale.y = 0.7
 
   let rectangle = new PIXI.Rectangle(0, 70, window.innerWidth, window.innerHeight);
-  let circle = new PIXI.Circle({
-    x: 50,
-    y: 50,
-    radius: 100
-  })
+  let circle = new PIXI.Circle(550, 550, 35)
   let catMessage = new Text("Meow!!!", new PIXI.TextStyle({
     fontFamily: "Arial",
     fontSize: 36,
     fill: "white",
-    // stroke: '#ff3300',
-    // strokeThickness: 4,
-    // dropShadow: true,
-    // dropShadowColor: "#000000",
-    // dropShadowBlur: 4,
-    // dropShadowAngle: Math.PI / 6,
-    // dropShadowDistance: 6,
+    stroke: '#ff3300',
+    strokeThickness: 4,
+    dropShadow: true,
+    dropShadowColor: "#000000",
+    dropShadowBlur: 4,
+    dropShadowAngle: Math.PI / 6,
+    dropShadowDistance: 6,
   }));
+
+  console.log(circle)
   
   document.body.appendChild(app.view)
 
   const container = new PIXI.Container()
-  // container.addChild(circle)
-  // container.addChild(catMessage)
-  container.addChild(catSprite)
+  container.addChild(circle, catMessage, catSprite)
+  
   app.stage.addChild(container)
 }
 
@@ -106,6 +82,7 @@ PIXI.loader
       url: meowSound
     }
   ])
+  .on("progress", (loader, resource) => console.log('loading', loader.progress))
   .load(init);
 
 
